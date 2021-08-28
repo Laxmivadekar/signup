7a02bd454f88d4e5d4d2e077b9749cc756dbbe06
Laxmivadekar <89687716+Laxmivadekar@users.noreply.github.com>
2021-08-28 22:25:43 +0530

    Add files via upload

---
+++ practice.py
@@ -0,0 +1,103 @@
+import json
+# import os
+main_dict={}
+list=[]
+dic_out={}
+user_info={}
+# if os.path.exists("userdetails.json"):
+#     pass
+# else:
+#     create=open("userdetails.json","w+")
+#     create.close()
+choose = int(input("select anyone of thet by number, 1.signup or 2.login:  "))
+if choose == 1:
+    username=input("enter the name of the user: ")
+    print('welcome',username,'for creating the account')
+    password=input("enter your password (password should conatain Upper,Small Letter, Special Character and number)=")
+    u,l,d,s=0,0,0,0 #upper,lower, digits,sum
+    for check in password:
+        if (check.isupper()):
+            u+=1
+        if (check.isdigit()):
+            d+=1
+        if (check.islower()):
+            l+=1
+        if(check=='@'or check=='$' or check=='_' or check=='#'):
+            s+=1
+    try:
+        with open("data.json","r") as output:
+            user_data=json.load(output)
+            for info in user_data["user"]:
+                pass
+    except:
+        pass
+    if (l>=1 and u>=1 and s>=1 and d>=1 and l+s+u+d==len(password)):
+        comform_password=input("enter your password again=")
+        if password==comform_password: 
+            if  ("data.json"):
+                print("Congractstulations!!!",username,"You are signed up Succesfully","\U0001F929")
+                description=input("Enter your Description: ")
+                birth_date=input("enter Your Date Of Birth: ")
+                Gender=input("enter your Gender: ")
+                hobbies=input("Enter Your hobby: ")
+                try:
+                    user_info["description"]=description
+                    user_info["d_o_b"]=birth_date
+                    user_info["Gender"]=Gender
+                    user_info["Hobbies"]=hobbies
+                    dic_out["Username"]=username
+                    dic_out["Password"]=password
+                    dic_out["Profile"]=user_info
+                    list.append(dic_out)
+                    main_dict["user"]=list
+                    with open("data.json","r+") as file:
+                        read_file= file.read()
+                        userdata=json.loads(read_file)
+                        for i in userdata:
+                            if i =="user":
+                                x=userdata[i]
+                                x.append(dic_out.copy())
+                                main_dict["user"]=x
+                                json.dumps(main_dict,file)
+                                file.close()
+                except:
+                    with open("data.json","w") as f:
+                        f.write(json.dumps(main_dict, indent=4))
+            else:
+                if info["Username"]!= username  or info["Password"]!= password:
+                    print("Congracts",username,"You are signed up Succesfully","\U0001F929")
+                    description=input("Enter your Description=")
+                    birth_date=input("enter Your Date Of Birth=")
+                    Gender=input("enter your Gender=")
+                    hobbies=input("Enter Your hobby=")
+                    try:
+                        user_info["description"]=description
+                        user_info["d_o_b"]=birth_date
+                        user_info["Gender"]=Gender
+                        user_info["Hobbies"]=hobbies
+                        dic_out["Username"]=username
+                        dic_out["Password"]=password
+                        dic_out["Profile"]=user_info
+                        list.append(dic_out)
+                        main_dict["user"]=list
+                        with open("data.json","r+") as file:
+                            read_file= file.read()
+                            userdata=json.loads(read_file)
+                            for i in userdata:
+                                if i =="user":
+                                    x=userdata[i]
+                                    x.append(dic_out.copy())
+                                    main_dict["user"]=x
+                                    json.dumps(main_dict,file)
+                                    file.close()
+                    except:
+                        with open("data.json","w") as f:
+                            f.write(json.dumps(main_dict, indent=4))
+                else:
+                    print("Your account is already Exsist!")
+        else:
+            print("both password are not same")
+    else:
+        print("At least password should contain one Specail number or one digit")
+else:
+    print('any data not found')
